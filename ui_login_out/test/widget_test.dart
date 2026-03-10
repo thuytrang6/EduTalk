ebca85c622855ed8acffcd4913fa63fccbfc1c30
Trang <nguyenthuytrang.6102005@gmail.com>
2026-03-10 16:58:23 +0700

    doi ten

--- ui_login_out/test/widget_test.dart
+++ ui_login_out/test/widget_test.dart
@@ -9,11 +9,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:ui_login_out/main.dart';
+import 'package:ui_login_out/screens/login_logout.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const Home());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
