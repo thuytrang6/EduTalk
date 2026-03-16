881f6dc09217b36f0bb41b9cb65520557c88e816
Trang <nguyenthuytrang.6102005@gmail.com>
2026-03-16 21:34:05 +0700

    update thêm 5 trang

---
+++ ui_login_out/lib/screens/LichSu.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class LichSuScreen extends StatelessWidget {
+  const LichSuScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text("Trang Lịch Sử", style: TextStyle(fontSize: 22)),
+    );
+  }
+}
---
+++ ui_login_out/lib/screens/ThaoLuan.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ThaoLuanScreen extends StatelessWidget {
+  const ThaoLuanScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text("Trang Thảo Luận", style: TextStyle(fontSize: 22)),
+    );
+  }
+}
