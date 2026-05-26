92a6eb5e013de88b269218fa2551a17052d3aec5
NguyenQuan <nguyenanhquan060205@gmail.com>
2026-05-26 20:22:22 +0700

    add model user

---
+++ ui_login_out/lib/models/user_model.dart
@@ -0,0 +1,46 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class UserModel {
+  final String uid;
+  final String email;
+  final String name;
+  final String role;
+  final Timestamp createdAt;
+  final bool isPremium;
+  final int usageCount;
+
+  UserModel({
+    required this.uid,
+    required this.email,
+    required this.name,
+    required this.role,
+    required this.createdAt,
+    this.isPremium = false,
+    this.usageCount = 0,
+  });
+
+  factory UserModel.fromDocument(DocumentSnapshot doc) {
+    final data = doc.data() as Map<String, dynamic>;
+    return UserModel(
+      uid: data['uid'] ?? '',
+      email: data['email'] ?? '',
+      name: data['name'] ?? '',
+      role: data['role'] ?? 'user',
+      createdAt: data['created_at'] ?? Timestamp.now(),
+      isPremium: data['isPremium'] ?? false,
+      usageCount: data['usageCount'] ?? 0,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'uid': uid,
+      'email': email,
+      'name': name,
+      'role': role,
+      'created_at': createdAt,
+      'isPremium': isPremium,
+      'usageCount': usageCount,
+    };
+  }
+}
